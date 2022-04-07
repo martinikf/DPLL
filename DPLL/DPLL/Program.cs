fd4df2539e81fb33b9d9fdcaee55156960616f71
Filip Martiník <martinik.filip01@gmail.com>
2022-04-07 13:09:58 +0200

    Init

---
+++ DPLL/DPLL/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Hello, World!");
+
+internal class Formula
+{
+    private List<Clause> clauses = new List<Clause>();
+
+
+    public void EvaluateLiteral(int i)
+    {
+        foreach (var clause in clauses)
+        {
+            if (clause.literals.Contains(i))
+            {
+                clauses.Remove(clause);
+            }
+            else
+            {
+                clause.literals.Remove(i);
+            }
+        }
+    }
+}
+
+internal class Clause
+{
+    public HashSet<int> literals = new HashSet<int>();
+}
